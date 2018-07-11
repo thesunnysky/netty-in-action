678259ffddd61bc38077cacb7e27c4b648c55feb
thesunnysky <jianyungsun@gmail.com>
2018-07-11 14:24:17 +0800

    udate

--- nettyfirst/src/main/java/org/sun/nettyfirst/DiscardServerHandler.java
+++ nettyfirst/src/main/java/org/sun/nettyfirst/DiscardServerHandler.java
@@ -1,8 +1,8 @@
 package org.sun.nettyfirst;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 import io.netty.util.ReferenceCountUtil;
 
@@ -11,7 +11,7 @@ import io.netty.util.ReferenceCountUtil;
  * ChannelHandlerAdapter， 这个类实现了ChannelHandler接口， ChannelHandler提供了许多事件处理的接口方法，
  * 然后你可以覆盖这些方法。 现在仅仅只需要继承ChannelHandlerAdapter类而不是你自己去实现接口方法。
  */
-public class DiscardServerHandler extends ChannelHandlerAdapter {
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
     /**
      * 这里我们覆盖了chanelRead()事件处理方法。 每当从客户端收到新的数据时， 这个方法会在收到消息时被调用，
      * 这个例子中，收到的消息的类型是ByteBuf
@@ -21,7 +21,6 @@ public class DiscardServerHandler extends ChannelHandlerAdapter {
      */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-
         try {
             ByteBuf in = (ByteBuf) msg;
             // 打印客户端输入，传输过来的的字符
